f7f9154a32b276679b9516812805db2c6e51888c
Marqvardi <marquinhos.vardi@gmail.com>
2018-08-08 21:24:46 +0100

    Commit Inicial

---
+++ Locadora/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Locadora
+{
+    partial class Formulario
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clienteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrarNovosClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procurarClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.midiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrarProdutoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procurarProdutoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alugarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtBoxNome = new System.Windows.Forms.TextBox();
+            this.txtBoxEndereco = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clienteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(112, 26);
+            // 
+            // clienteToolStripMenuItem
+            // 
+            this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clienteToolStripMenuItem1,
+            this.midiaToolStripMenuItem,
+            this.alugarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clienteToolStripMenuItem1
+            // 
+            this.clienteToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrarNovosClientesToolStripMenuItem,
+            this.procurarClientesToolStripMenuItem});
+            this.clienteToolStripMenuItem1.Name = "clienteToolStripMenuItem1";
+            this.clienteToolStripMenuItem1.Size = new System.Drawing.Size(56, 20);
+            this.clienteToolStripMenuItem1.Text = "Cliente";
+            // 
+            // cadastrarNovosClientesToolStripMenuItem
+            // 
+            this.cadastrarNovosClientesToolStripMenuItem.Name = "cadastrarNovosClientesToolStripMenuItem";
+            this.cadastrarNovosClientesToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.cadastrarNovosClientesToolStripMenuItem.Text = "Cadastrar novos clientes";
+            this.cadastrarNovosClientesToolStripMenuItem.Click += new System.EventHandler(this.cadastrarNovosClientesToolStripMenuItem_Click);
+            // 
+            // procurarClientesToolStripMenuItem
+            // 
+            this.procurarClientesToolStripMenuItem.Name = "procurarClientesToolStripMenuItem";
+            this.procurarClientesToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.procurarClientesToolStripMenuItem.Text = "Procurar clientes";
+            // 
+            // midiaToolStripMenuItem
+            // 
+            this.midiaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrarProdutoToolStripMenuItem,
+            this.procurarProdutoToolStripMenuItem});
+            this.midiaToolStripMenuItem.Name = "midiaToolStripMenuItem";
+            this.midiaToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.midiaToolStripMenuItem.Text = "Midia";
+            // 
+            // cadastrarProdutoToolStripMenuItem
+            // 
+            this.cadastrarProdutoToolStripMenuItem.Name = "cadastrarProdutoToolStripMenuItem";
+            this.cadastrarProdutoToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.cadastrarProdutoToolStripMenuItem.Text = "Cadastrar produto";
+            // 
+            // procurarProdutoToolStripMenuItem
+            // 
+            this.procurarProdutoToolStripMenuItem.Name = "procurarProdutoToolStripMenuItem";
+            this.procurarProdutoToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.procurarProdutoToolStripMenuItem.Text = "Procurar produto";
+            // 
+            // alugarToolStripMenuItem
+            // 
+            this.alugarToolStripMenuItem.Name = "alugarToolStripMenuItem";
+            this.alugarToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.alugarToolStripMenuItem.Text = "Alugar";
+            // 
+            // txtBoxNome
+            // 
+            this.txtBoxNome.Location = new System.Drawing.Point(251, 68);
+            this.txtBoxNome.Name = "txtBoxNome";
+            this.txtBoxNome.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxNome.TabIndex = 2;
+            this.txtBoxNome.Text = "w";
+            // 
+            // txtBoxEndereco
+            // 
+            this.txtBoxEndereco.Location = new System.Drawing.Point(404, 67);
+            this.txtBoxEndereco.Name = "txtBoxEndereco";
+            this.txtBoxEndereco.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxEndereco.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBoxEndereco);
+            this.Controls.Add(this.txtBoxNome);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Formulario";
+            this.Text = "Locadora";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cadastrarNovosClientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procurarClientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem midiaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastrarProdutoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procurarProdutoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alugarToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtBoxNome;
+        private System.Windows.Forms.TextBox txtBoxEndereco;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Locadora/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Locadora
+{
+    public partial class Formulario : Form
+    {
+                        
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void testeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cadastrarNovosClientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConectarSQLClass cp = new ConectarSQLClass();
+            cp.ConectarSQL();
+            cp.InsertSQL(txtBoxNome.Text, txtBoxEndereco.Text);
+            cp.DesconectarSQL();        
+
+        }
+    }
+}
